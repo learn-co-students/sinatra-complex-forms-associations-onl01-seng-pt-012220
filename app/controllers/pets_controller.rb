e0b1ca4e6fa1ca220e95d2dc82d74babd32227b0
Jonathan Ghandforoush <jonnyghan@gmail.com>
2020-05-07 03:36:18 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,4 @@
-class PetsController < ApplicationController
+ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
@@ -6,16 +6,32 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners= Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
-    redirect to "pets/#{@pet.id}"
+  @pets = Pet.create(params[:pet])
+  @owner= Owner.create(params[:owner])
+  
+ # binding.pry
+  if @pets.owner_id == nil
+      @pets.owner_id = @owner.id 
+      @pets.save
+    end 
+    #binding.pry
+    redirect to "pets/#{@pets.id}"
   end
 
+get '/pets/:id/edit' do
+    @pet = Pet.find_by_id(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
+  
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
+   # binding.pry
     erb :'/pets/show'
   end
 
