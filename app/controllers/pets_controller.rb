7ca372e5c69e277511534a5299bb374f5697fe53
skj1991 <stacykjohns@icloud.com>
2020-10-24 20:37:52 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,49 +2,40 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-    #binding.pry
-    @pet = Pet.new
-    @pet.name = params[:pet_name]
-    #binding.pry
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
     if !params["owner"]["name"].empty?
-      owner = Owner.create(name: params["owner"]["name"])
-      @pet.owner_id = owner.id
+      @pet.update(owner: Owner.create(name: params["owner"]["name"]))
     end
-    #binding.pry
-    redirect to "pets/#{@pet.id}"
+    redirect to "/pets/#{@pet.id}"
   end
 
-  get '/pets/:id/edit' do 
-    @owners = Owner.all
-    @pet = Pet.find(params[:id])
-    erb :'/pets/edit'
-  end
-  
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
-    if !params[:pet].keys.include?("owner_ids")
-      params[:pet]["owner_ids"] = []
-    end
-   
-      @pet = Pet.find(params[:id])
-      @pet.update(params["pet"])
-      if !params["owner"]["name"].empty?
-        @pet.owners << Owner.create(name: params["owner"]["name"])
-      end
-
-    redirect to "pets/#{@pet.id}"
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
   end
+
+  patch '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+    if !params["owner"]["name"].empty?
+      @pet.update(owner: Owner.create(name: params["owner"]["name"]))
+    end
+    redirect to "/pets/#{@pet.id}"
+  end
+
 end
